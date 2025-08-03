406a52d1ab37592d99fbffb024324133134d096b
ruslan <ru_a@mail.ru>
2025-08-03 18:34:38 +0300

    add "Первое создание классов и объектов"

---
+++ express_java/src/main/java/practice_2/Book.java
@@ -0,0 +1,38 @@
+package practice_2;
+
+public class Book {
+    //поля
+    String title;
+    String author;
+
+    //конструктор
+    Book (String title,String author) {
+        this.title = title;
+        this.author = author;
+    }
+    //геттеры
+    String getTitle(){
+        return title;
+    }
+    String getAuthor(){
+        return author;
+    }
+    //сетттеры
+    void setTitle(String title){
+        this.title = title;
+    }
+
+    void setAuthor(String author){
+        this.author = author;
+    }
+    //метод на печать
+    void print (){
+        System.out.println(title +", "+ author);
+    }
+
+}
+
+//Создайте класс Book с полями title и author.
+//        Реализуйте конструктор, геттеры и сеттеры, и метод printInfo(),
+//выводящий название и автора книги.
+//В main создайте книгу, измените автора и выведите информацию.
